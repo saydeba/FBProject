671c9cb5f5d69380beb6ea4c37b636f1a3c0b06a
SAYAK <SAYAK@DESKTOP-SAYAK>
2019-08-03 19:53:07 +0530

    master file added

---
+++ com.saydeba/src/main/java/com/saydeba/com/saydeba/masterFile.java
@@ -0,0 +1,14 @@
+package com.saydeba.com.saydeba;
+
+public class masterFile {
+
+	public masterFile() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
