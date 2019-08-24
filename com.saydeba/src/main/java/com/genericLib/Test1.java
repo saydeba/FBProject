b6fee91cadedaacaeffdf0bb77ada55177760e5a
iamdr <iamdr@DESKTOP-OM70CMO>
2019-08-24 18:50:48 +0530

    commit Test1 and baseclassTest to branchdip

---
+++ com.saydeba/src/main/java/com/genericLib/Test1.java
@@ -0,0 +1,19 @@
+package com.genericLib;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		if(true)
+		{
+		int i=4;
+		int j=5;
+		System.out.println(i+j);
+		}
+//		else
+//		{
+//			System.out.println("wrong ans");
+//		}
+
+	}
+
+}
