0dd99a1452b212f616278677ef6f639e0f84b2de
SAYAK <SAYAK@DESKTOP-SAYAK>
2019-09-08 21:27:30 +0530

    added

--- com.saydeba/src.FB/com/testCases/FbSignUp.java
+++ com.saydeba/src.FB/com/testCases/FbSignUp.java
@@ -1,6 +1,8 @@
 package com.testCases;
 
 import org.testng.annotations.Test;
+
+import com.ObjectRepository.FbXpath;
 import com.genericLib.BaseClass;
 import com.genericLib.ExcelDataConfig;
 import com.genericLib.FileUtils;
@@ -11,11 +13,18 @@ public class FbSignUp extends BaseClass
 	FileUtils excelConfig = new FileUtils();
 	ExcelDataConfig excel = new ExcelDataConfig(excelConfig.getExcel());
 	
-@Test
-	public String getFName()
+	@Test
+	public void xPathClick() throws Exception
 	{
+		FbXpath fbxp = new FbXpath();
+		fbxp.nameFirst();
+	}
+	
+	public String getFName() throws Exception
+	{
+		
 		System.out.println("vvvvvvvvvvvvvvvvvvvvv");
-		String nmm = excel.getData(0, 1, 0);
-		return nmm;
+		return excel.getData(0, 1, 0);
+		
 	}
 }
