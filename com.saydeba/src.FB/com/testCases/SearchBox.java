42fc82721a789e37f8e0dd86e04c4115d8a2f368
saydeba <53596961+saydeba@users.noreply.github.com>
2019-09-10 11:00:30 +0530

    Create SearchBox.java

---
+++ com.saydeba/src.FB/com/testCases/SearchBox.java
@@ -0,0 +1,19 @@
+package com.testCases;
+
+import org.testng.annotations.Test;
+import com.ObjectRepository.FbXpath;
+import com.genericLib.BaseClass;
+
+
+public class SearchBox extends BaseClass
+{
+	@Test
+	public void searchName() throws Exception 
+	{
+		FbXpath xpath = new FbXpath();
+		
+		Thread.sleep(500);
+		
+		
+	}
+}
