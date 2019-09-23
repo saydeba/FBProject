9d1acd6a3fc58dbb8bf0b505ec4db76bccdb3e28
SAYAK <SAYAK@DESKTOP-SAYAK>
2019-09-23 19:33:30 +0530

    Add cover photo file

--- com.saydeba/src.FB/com/testCases/AddCoverPhoto.java
+++ com.saydeba/src.FB/com/testCases/AddCoverPhoto.java
@@ -1,47 +1,141 @@
 package com.testCases;
 
+import java.util.Iterator;
+import java.util.Set;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import com.genericLib.*;
 
 public class AddCoverPhoto extends BaseClass
 {
 	SayProfilePage sfp = new SayProfilePage();
+	WebDriverCommonUtils wdcu = new WebDriverCommonUtils();
 	
 	@Test
 	public void addcover() throws Exception
 	{
 		sfp.profile();
-		wdriver.findElement(By.xpath("//span[contains(text(),'Add Cover')]")).click();
-
-		String nm="Your cover photo";
-		if(nm.equals("Your cover photo"))
-		{                          
-			wdriver.findElement(By.xpath("//a[contains(text(),'Skip')]")).click();
-			wdriver.findElement(By.xpath("//div[@id='fbProfileCoverPhotoSelector']")).click();
-			//System.out.println("SKIP");
-			Thread.sleep(300);
-
+		System.out.println("under addcover method... after Add cover photo click");
+		wdcu.waitForPageToLoad();
+							
+					//Screen for Your cover photo --> OK or SKIP or Direct
+		wdriver.findElement(By.xpath("//span[contains(text(),'Add Cover Photo')]")).click();
+		
+		String nm="Your cover photo"; 
+		if(nm.equals("Your cover photo")){
+		switch(nm)
+		//if(nm.equals("Your cover photo"))
+		{   
+		case "one":
+			wdriver.findElement(By.xpath("//a[contains(text(),'Skip')]")).click();  // for SKIP
+			wdriver.findElement(By.xpath("//span[contains(text(),'Add Cover Photo')]")).click();
+			System.out.println("SKIP");
+			wdriver.findElement(By.xpath("//span[contains(text(), 'Select photo')]")).click();
+			Thread.sleep(500);
+		
+		//else if(nm.equals("Your cover photo"))
+		case "two":
+			wdriver.findElement(By.xpath("//button[contains(text(),'OK')]")).click(); // for OK
+			//wdriver.findElement(By.xpath("//span[contains(text(),'Add Cover Photo')]")).click();
+			System.out.println("OK");
+			wdriver.findElement(By.xpath("//span[contains(text(), 'Select photo')]")).click();
+			Thread.sleep(500);
+		
+		//else
+		case "three":
+			System.out.println("Without OK and Skip");
+			//wdriver.findElement(By.xpath("//span[contains(text(),'Add Cover Photo')]")).click(); // for direct
+			System.out.println("Add Cover Photo click inside");
+			wdriver.findElement(By.xpath("//span[contains(text(), 'Select photo')]")).click();
+			Thread.sleep(500);
+		
+			default:
+				System.out.println("Default in Switch case");
+				wdriver.findElement(By.xpath("//span[contains(text(), 'Select photo')]")).click();
 		}
-		else if(nm.equals("Your cover photo"))
+		//Thread.sleep(1500);
+		} 
+		//==================Select photo clicking....
+//		wdriver.findElement(By.xpath("//span[contains(text(), 'Select photo')]")).click();
+//		Thread.sleep(5000);
+//		wdriver.findElement(By.xpath("//span[contains(text(), 'Select artwork')]")).click();
+//		Thread.sleep(5000);
+//		wdriver.findElement(By.xpath("//label[contains(text(), 'Upload photo...')]")).click();
+//		Thread.sleep(5000);
+		
+		String selectPhoto = "Choose From My Photos";
+		String selectArtwork ="Select artwork";
+		String uploadPhoto = "Upload photo...";
+		
+		if(selectPhoto.equals("Choose From My Photos"))
 		{
-			wdriver.findElement(By.xpath("//button[contains(text(),'OK')]")).click();
-			wdriver.findElement(By.xpath("//div[@id='fbProfileCoverPhotoSelector']")).click();
-			//System.out.println("OK");
-			Thread.sleep(300);
-
+			selectPhoto();
 		}
-		else
+		else if(selectArtwork.equals("Select artwork"))
 		{
-			wdriver.findElement(By.xpath("//div[@id='fbProfileCoverPhotoSelector']")).click();
-			//System.out.println("Add Cover Photo click inside");
-			Thread.sleep(300);
+			selectArtwork();
 		}
-
-		//	wdriver.findElement(By.xpath("//div[@id='fbProfileCoverPhotoSelector']")).click();
-		//	System.out.println("Add Cover Photo click outside");
-
+		else if(uploadPhoto.equals("Upload photo..."))
+		{
+			uploadPhoto();
+		}
 	}
+
+	private void selectPhoto() throws Exception 
+	{
+		System.out.println("In the selectPhoto method");
+		String parentWindowHandler = wdriver.getWindowHandle(); // Store your parent window
+		String subWindowHandler = null;
+
+		Set<String> handles = wdriver.getWindowHandles(); // get all window handles
+		Iterator<String> iterator = handles.iterator();
+		while (iterator.hasNext()){
+		    subWindowHandler = iterator.next();
+		}
+		wdriver.switchTo().window(subWindowHandler); // switch to popup window
+	
+			// in the popup window, perform necessary actions ....
+       
+		System.out.println("Select photo link clicked.....");
+		String rp= "Recent Photos";
+		if(rp.equals("Recent Photos"))
+		{
+			wdriver.findElement(By.xpath("//div[@class='uiScaledImageContainer'][1]")).click();
+			  // Click on the photo for "New popup Window"
+			System.out.println("photo clicked.....");
+			//wdriver.findElement(By.xpath("//span[contains(text(), 'Photo albums')]")).click();
+			//wdcu.waitForPageToLoad();
+			Thread.sleep(800);
+			//wdriver.findElement(By.xpath("//button[contains(text(), 'Cancel')]")).click();
+			wdriver.findElement(By.xpath("//i[@class='img sp_XvdHefsjITT sx_aa0b1e')]")).click();
+			wdriver.switchTo().window(parentWindowHandler);  // switch back to parent window
+	        wdriver.findElement(By.xpath("//span[contains(text(),'Add Cover Photo')]")).click();
+	        selectArtwork();
+		}
+		else	
+			wdriver.findElement(By.xpath("//a[contains(text(), 'Cancel')]")).click();
+			wdriver.findElement(By.xpath("//span[contains(text(),'Add Cover Photo')]")).click();
+//		wdriver.findElement(By.xpath("//a[contains(text(), 'Cancel')]")).click();
+		
+	}
+	
+	private void selectArtwork() 
+	{
+		
+		System.out.println("Select Artwork link clicked.....");
+	}
+
+	private void uploadPhoto()
+	{
+		System.out.println("Select photo page clicked.....");
+		
+	}
+	
 }
+
+
+//       //a[contains(text(),'Update')]
 
 
